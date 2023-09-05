603f8d12925433115230e419fb4bb6442f5dd96e
hamidwise1 <hrawebdev@gmail.com>
2023-09-05 17:04:23 +0430

    add store reducer + add loader component + add error component + add start screen component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import './index.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import Loader from './components/Loader';
+import Error from './components/Error';
+import StartScreen from './components/StartScreen';
+
+import { useEffect, useReducer } from 'react';
+const initialStore = {
+ questions: [],
+ status: 'ready',
+};
+
+const storeReducer = (state, { type, payload }) => {
+ if (type === 'SET_DATA') {
+  return {
+   ...state,
+   question: payload,
+  };
+ }
+ if (type === 'CHANGE_STATUS') {
+  return {
+   ...state,
+   status: payload,
+  };
+ }
+
+ return { ...state };
+};
 
 const App = () => {
+ const [store, dispatch] = useReducer(storeReducer, initialStore);
+ const { status, questions } = store;
+
+ const getQusetions = async () => {
+  dispatch({ type: 'CHANGE_STATUS', payload: 'loading' });
+  try {
+   const result = await fetch('http://localhost:8000/questions');
+   const data = await result.json();
+   dispatch({ type: 'CHANGE_STATUS', payload: 'ready' });
+   dispatch({ type: 'SET_DATA', payload: data });
+  } catch (err) {
+   dispatch({ type: 'CHANGE_STATUS', payload: 'error' });
+  }
+ };
+
+ useEffect(() => {
+  getQusetions();
+ }, []);
+
  return (
   <div className='app'>
    <Header />
    <Main>
-    <p>1/15</p>
-    <p>question</p>
+    {status === 'loading' && <Loader />}
+    {status === 'error' && <Error />}
+    {status === 'ready' && <StartScreen />}
    </Main>
   </div>
  );
---
+++ src/components/StartScreen.js
@@ -0,0 +1,11 @@
+const StartScreen = () => {
+ return (
+  <div className='start'>
+   <h2>welcome to the react quiz !</h2>
+   <h3>x qestion to test your mastery</h3>
+   <button>lets start</button>
+  </div>
+ );
+};
+
+export default StartScreen;
